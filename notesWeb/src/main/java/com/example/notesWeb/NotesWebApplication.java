9b8252ae8403db7e41b381d6dac28799afb23c62
nhannguyen <thanhnhanna102003@gmail.com>
2025-07-04 22:20:01 +0700

    PushCodeGit

---
+++ notesWeb/src/main/java/com/example/notesWeb/NotesWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.notesWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotesWebApplication.class, args);
+	}
+
+}
---
+++ notesWeb/src/test/java/com/example/notesWeb/NotesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notesWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
