a7d3d9ed0c057f66c76b0207564d05e3b93ecb35
nhannguyen <thanhnhanna102003@gmail.com>
2025-07-28 23:45:24 +0700

    Fix Logic Repo&Service Register User

--- notesWeb/src/main/java/com/example/notesWeb/NotesWebApplication.java
+++ notesWeb/src/main/java/com/example/notesWeb/NotesWebApplication.java
@@ -7,7 +7,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NotesWebApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(NotesWebApplication.class, args);
+
+		SpringApplication.run(
+				NotesWebApplication.class, args);
 	}
 
 }
