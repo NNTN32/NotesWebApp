ce4b2479d24772c57493b5b22ebf94df2ca3b879
nhannguyen <thanhnhanna102003@gmail.com>
2025-08-14 00:26:26 +0700

    Add Logic Sending data request login to Redis handle it

---
+++ notesWeb/src/main/java/com/example/notesWeb/exception/AuthRedisProducer.java
@@ -0,0 +1,35 @@
+package com.example.notesWeb.exception;
+
+import com.example.notesWeb.dtos.AuthRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.stream.StreamRecords;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AuthRedisProducer {
+    @Autowired
+    private RedisTemplate<String , Object> redisTemplate;
+
+    private static final String STREAM_KEY = "auth:login:stream";
+
+    public void sendLoginRequest(AuthRequest authRequest){
+        //Using hashmap for key String & any value = object
+        Map<String, Object> data = new HashMap<>();
+        data.put("username", authRequest.getUsername());
+        data.put("password", authRequest.getPassword());
+        data.put("sessionId", authRequest.getSessionID());
+
+        //Log check result
+        System.out.println("Sending request to Redis Stream: " + data);
+        redisTemplate.opsForStream()
+                .add(StreamRecords.newRecord()
+                        .in("auth:login:stream")
+                        .ofMap(data));
+    }
+}
