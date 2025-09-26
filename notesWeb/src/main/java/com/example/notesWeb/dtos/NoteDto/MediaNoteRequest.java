7aecd49a951bdb205f1fa9b198d445b007c92850
nhannguyen <thanhnhanna102003@gmail.com>
2025-09-26 14:53:21 +0700

    Add Logic Class Service Upload Media Notes Using Cloudinary Define Type of Media & Change Class DTO & Add Interface Media Repo

--- notesWeb/src/main/java/com/example/notesWeb/dtos/NoteDto/MediaNoteRequest.java
+++ notesWeb/src/main/java/com/example/notesWeb/dtos/NoteDto/MediaNoteRequest.java
@@ -4,11 +4,12 @@ import com.example.notesWeb.model.takeNotes.MediaType;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MediaNoteRequest {
-    private String url;
+    private MultipartFile file;
     private MediaType mediaType;
 }
