e8191191469c9e11796534bb80f5d72e45cee2f4
nhannguyen <thanhnhanna102003@gmail.com>
2025-09-27 19:47:23 +0700

    Add logic get list all notes at service class

---
+++ notesWeb/src/main/java/com/example/notesWeb/service/takeNotes/TaskNoteService.java
@@ -0,0 +1,28 @@
+package com.example.notesWeb.service.takeNotes;
+
+import com.example.notesWeb.dtos.NoteDto.NoteResponse;
+import com.example.notesWeb.model.takeNotes.Notes;
+import com.example.notesWeb.repository.MediaRepo;
+import com.example.notesWeb.repository.NotesRepo;
+import com.example.notesWeb.repository.UserRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Sort;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TaskNoteService {
+    private final UserRepo userRepo;
+    private final NotesRepo notesRepo;
+    private final MediaRepo mediaRepo;
+
+    //Logic take lists Note
+    public List<Notes> getAllListNote(){
+        return notesRepo.findAll(Sort.by(Sort.Direction.DESC,"createdAt"));
+    }
+
+
+}
