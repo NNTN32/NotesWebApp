10ead529cc651edf2e3922d3dff190045e741e65
nhannguyen <thanhnhanna102003@gmail.com>
2025-09-29 22:59:38 +0700

    Write QUERY at class interface NotesRepo JOIN Table

--- notesWeb/src/main/java/com/example/notesWeb/repository/NotesRepo.java
+++ notesWeb/src/main/java/com/example/notesWeb/repository/NotesRepo.java
@@ -2,10 +2,13 @@ package com.example.notesWeb.repository;
 
 import com.example.notesWeb.model.takeNotes.Notes;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
 
 public interface NotesRepo extends JpaRepository<Notes, Long> {
     List<Notes> findByUserId(Long userID);
+    @Query("SELECT * FROM notes JOIN note_media ON notes.id = note_media.note_id")
+    List<Notes> findNoteId(Long noteID);
 }
