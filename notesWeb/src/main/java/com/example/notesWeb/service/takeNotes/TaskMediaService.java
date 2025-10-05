9333736521e4c465025bda435f593c878dde2e67
nhannguyen <thanhnhanna102003@gmail.com>
2025-10-06 00:08:18 +0700

    Add new logic delete File Media Notes at new service class TaskMediaService

---
+++ notesWeb/src/main/java/com/example/notesWeb/service/takeNotes/TaskMediaService.java
@@ -0,0 +1,26 @@
+package com.example.notesWeb.service.takeNotes;
+
+import com.example.notesWeb.model.takeNotes.NoteMedia;
+import com.example.notesWeb.model.takeNotes.Notes;
+import com.example.notesWeb.repository.MediaRepo;
+import com.example.notesWeb.repository.NotesRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TaskMediaService {
+    private final NotesRepo notesRepo;
+    private final MediaRepo mediaRepo;
+
+    //Logic handle delete Media of notes
+    public void deleteMedia(Long noteId, Long mediaID){
+        NoteMedia noteMedia = mediaRepo.findById(mediaID)
+                .orElseThrow(() -> new IllegalArgumentException("File haven't uploaded yet! " + mediaID));
+
+        if(!noteMedia.getNotes().getId().equals(noteId)){
+            throw new IllegalArgumentException("Can not delete file !");
+        }
+        mediaRepo.delete(noteMedia);
+    }
+}
