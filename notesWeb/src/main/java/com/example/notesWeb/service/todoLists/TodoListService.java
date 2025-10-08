c2fbdb8932e8c9aae6a1d1ea7dca763f9008128e
nhannguyen <thanhnhanna102003@gmail.com>
2025-10-09 01:02:48 +0700

    Change Dto class ListRequest about delete entity State, Add logic create todo lists, set up state for it & update state for todo list

---
+++ notesWeb/src/main/java/com/example/notesWeb/service/todoLists/TodoListService.java
@@ -0,0 +1,48 @@
+package com.example.notesWeb.service.todoLists;
+
+
+import com.example.notesWeb.dtos.TodoListDto.ListRequest;
+import com.example.notesWeb.model.User;
+import com.example.notesWeb.model.todoLists.ListTodo;
+import com.example.notesWeb.model.todoLists.State;
+import com.example.notesWeb.repository.UserRepo;
+import com.example.notesWeb.repository.todoRepo.TodoRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TodoListService {
+    private final UserRepo userRepo;
+    private final TodoRepo todoRepo;
+
+    //Logic create list to do for users
+    public ListTodo createList(ListRequest listRequest, Long userId){
+        User user = userRepo.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with ID:" + userId));
+
+        ListTodo listTodo = new ListTodo();
+        listTodo.setHeading(listRequest.getHeading());
+        listTodo.setPurport(listRequest.getPurport());
+        listTodo.setUser(user);
+
+        listTodo.setState(State.FAIL);
+        return todoRepo.save(listTodo);
+    }
+
+    //Logic update status of to do list
+    public ListTodo markDone(Long todoID, Long userid){
+        ListTodo todo = todoRepo.findById(todoID)
+                .orElseThrow(() -> new IllegalArgumentException("Todo not found" + todoID));
+
+        if(!todo.getUser().getId().equals(userid)){
+            throw new AccessDeniedException("Not your todo");
+        }
+
+        todo.setState(State.CORRECT);
+        return todoRepo.save(todo);
+    }
+
+}
