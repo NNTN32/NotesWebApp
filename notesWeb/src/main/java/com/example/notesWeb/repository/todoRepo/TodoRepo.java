bab65861746aec0029d22c8e1a11b4854cb1b0c1
nhannguyen <thanhnhanna102003@gmail.com>
2025-10-10 01:53:37 +0700

    Add logic get list todo of user based on ID

--- notesWeb/src/main/java/com/example/notesWeb/repository/todoRepo/TodoRepo.java
+++ notesWeb/src/main/java/com/example/notesWeb/repository/todoRepo/TodoRepo.java
@@ -3,5 +3,8 @@ package com.example.notesWeb.repository.todoRepo;
 import com.example.notesWeb.model.todoLists.ListTodo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TodoRepo extends JpaRepository<ListTodo, Long> {
+    List<ListTodo> findTodoListOfUserId(Long userId);
 }
---
+++ notesWeb/src/main/java/com/example/notesWeb/service/todoLists/TaskListService.java
@@ -0,0 +1,30 @@
+package com.example.notesWeb.service.todoLists;
+
+
+import com.example.notesWeb.model.User;
+import com.example.notesWeb.model.todoLists.ListTodo;
+import com.example.notesWeb.repository.UserRepo;
+import com.example.notesWeb.repository.todoRepo.TodoRepo;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TaskListService {
+    private final UserRepo userRepo;
+    private final TodoRepo todoRepo;
+
+    //Logic get list to do of users
+    public List<ListTodo> getAllTodoList(Long userId){
+        User user = userRepo.findById(userId)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with ID:" + userId));
+        try{
+            return todoRepo.findTodoListOfUserId(user.getId());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
