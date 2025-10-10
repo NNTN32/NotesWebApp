0ec34b7121377c46ca9818ebbdbc9a35fbaa7a98
nhannguyen <thanhnhanna102003@gmail.com>
2025-10-11 00:25:09 +0700

    Add logic handle update to do lists of User

--- notesWeb/src/main/java/com/example/notesWeb/service/todoLists/TaskListService.java
+++ notesWeb/src/main/java/com/example/notesWeb/service/todoLists/TaskListService.java
@@ -1,6 +1,7 @@
 package com.example.notesWeb.service.todoLists;
 
 
+import com.example.notesWeb.dtos.TodoListDto.ListRequest;
 import com.example.notesWeb.model.User;
 import com.example.notesWeb.model.todoLists.ListTodo;
 import com.example.notesWeb.repository.UserRepo;
@@ -10,11 +11,14 @@ import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
 import java.util.List;
+
+import static com.example.notesWeb.model.todoLists.State.FAIL;
 
 @Service
 @RequiredArgsConstructor
-public class TaskListService {
+public class  TaskListService {
     private final UserRepo userRepo;
     private final TodoRepo todoRepo;
 
@@ -39,5 +43,34 @@ public class TaskListService {
         }
 
         todoRepo.delete(listTodo);
+    }
+
+    //Logic update to do lists
+    public ListTodo updateLists(ListRequest listRequest, Long idList, Long idUser){
+        User user = userRepo.findById(idUser)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with ID:" + idUser));
+
+        ListTodo todo = todoRepo.findById(idList)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found with ID:" + idList));
+
+        try{
+            if (!todo.getUser().getId().equals(user.getId())){
+                throw new AccessDeniedException("You're not authorized to update this Lists!");
+            }
+
+            if(todo.getState() == FAIL){
+                if(listRequest.getHeading() != null && !listRequest.getHeading().isBlank()){
+                    todo.setHeading(listRequest.getHeading());
+                }
+                if(listRequest.getPurport() != null && !listRequest.getPurport().isBlank()){
+                    todo.setPurport(listRequest.getPurport());
+                }
+            }
+
+            todo.setUpdatedAt(LocalDateTime.now());
+            return todoRepo.save(todo);
+        }catch (Exception e){
+            throw new RuntimeException("Failed to update Notes! " + e.getMessage());
+        }
     }
 }
