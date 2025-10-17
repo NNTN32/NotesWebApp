fd8a8ee04afaede7e49283be6ff411a0ecbfcd1a
nhannguyen <thanhnhanna102003@gmail.com>
2025-10-17 14:58:08 +0700

    Optimize class AuthRedisConsumer by using ThreadPoolExecutor with queue for parallel processing and rate limit request handling

--- notesWeb/src/main/java/com/example/notesWeb/exception/AuthRedisConsumer.java
+++ notesWeb/src/main/java/com/example/notesWeb/exception/AuthRedisConsumer.java
@@ -16,7 +16,7 @@ import java.time.Duration;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 @Component
 @RequiredArgsConstructor
@@ -29,6 +29,19 @@ public class AuthRedisConsumer {
 
     @Autowired
     private SimpMessagingTemplate messagingTemplate;
+
+    //Using threadpool handle multiple login requests in parallel
+    private final ThreadPoolExecutor executorService = new ThreadPoolExecutor(
+            10,                              // corePoolSize: 10 main threads
+            30,                                         // maxPoolSize: up to 30 threads at high load
+            60L,                                        // idle thread timeout
+            TimeUnit.SECONDS,
+            new LinkedBlockingQueue<>(200),    //Queue contains 200 requests waiting to be processed
+            new ThreadPoolExecutor.CallerRunsPolicy()  //If queue is full then run on main thread
+    );
+
+    //Semaphore for handle limit speed request
+    private final Semaphore rateLimiter = new Semaphore(20); //Commit maximize 20 request/sec
 
     private static final String STREAM_KEY = "auth:login:stream";
     private static final String GROUP = "auth-group";
@@ -45,66 +58,87 @@ public class AuthRedisConsumer {
         //Create thread of stream can read request message from client
         Executors.newSingleThreadExecutor().submit(() -> {
             while (true){
-                List<MapRecord<String, Object, Object>> records = redisTemplate.opsForStream()
-                        .read(Consumer.from(GROUP, CONSUMER_NAME),
-                                //Read about 1s each 10 times request message
-                                StreamReadOptions.empty().count(10).block(Duration.ofSeconds(1)),
-                                //Read the last message caching not yet handle request
-                                StreamOffset.create(STREAM_KEY, ReadOffset.lastConsumed()));
+                try{
+                    List<MapRecord<String, Object, Object>> records = redisTemplate.opsForStream()
+                            .read(Consumer.from(GROUP, CONSUMER_NAME),
+                                    //Read about 1s each 10 times request message
+                                    StreamReadOptions.empty().count(10).block(Duration.ofSeconds(1)),
+                                    //Read the last message caching not yet handle request
+                                    StreamOffset.create(STREAM_KEY, ReadOffset.lastConsumed()));
 
-                //Logic handle new message request from client
-                if(records != null){
+                    if(records == null || records.isEmpty()) continue;
+
                     for(MapRecord<String, Object, Object> record : records){
-                        String username = (String) record.getValue().get("username");
-                        String password = (String) record.getValue().get("password");
-                        String sessionId = (String) record.getValue().get("sessionId");
-
-                        //Handle auth service taken data user
-                        try{
-                            AuthRequest request = new AuthRequest(username, password, sessionId);
-                            AuthResponse response = authService.login(request);
-
-                            messagingTemplate.convertAndSendToUser(sessionId, "/queue/login-result", response);
-
-                            //Save result into redis hash for service & api can read, handle
-                            String resultKey = "login:result:" + sessionId;
-
-                            //Using HashMap class Dto
-                            Map<String, String> resultMap = new HashMap<>();
-                            resultMap.put("status", response.getStatus().toString());
-                            resultMap.put("username", response.getUsername());
-                            resultMap.put("token", response.getToken());
-                            resultMap.put("id", String.valueOf(response.getId()));
-                            resultMap.put("role", String.valueOf(response.getRole()));
-                            resultMap.put("message", response.getMessage());
-
-                            redisTemplate.opsForHash().putAll(resultKey, resultMap);
-                            //Setting time expired for redis
-                            redisTemplate.expire(resultKey,Duration.ofMinutes(1));
-                        } catch (Exception e) {
-                            AuthResponse errorResponse = new AuthResponse(
-                                    null,null, username, null, Status.FAIL, e.getMessage());
-
-                            messagingTemplate.convertAndSendToUser(sessionId, "queue/login-result", errorResponse);
-
-                            //Save fail result into Redis
-                            String resultKey = "login:result" + sessionId;
-                            Map<String, String> resultMap = new HashMap<>();
-                            resultMap.put("status", Status.FAIL.toString());
-                            resultMap.put("username", username);
-                            resultMap.put("token", "");
-                            resultMap.put("id", "");
-                            resultMap.put("role", "");
-                            resultMap.put("message", e.getMessage());
-
-                            redisTemplate.opsForHash().putAll(resultKey, resultMap);
-                            redisTemplate.expire(resultKey, Duration.ofMinutes(1));
-
-                        }
-                        redisTemplate.opsForStream().acknowledge(STREAM_KEY, GROUP, record.getId());
+                        executorService.submit(() -> handleAuthRecord(record));
                     }
+                } catch (Exception e) {
+                    System.err.println("Stream read error: " + e.getMessage());
                 }
             }
         });
+    }
+
+    //Logic handle message request Login from client
+    private void handleAuthRecord(MapRecord<String, Object, Object> record){
+        boolean permit = rateLimiter.tryAcquire();
+        if(!permit) {
+            System.err.println("Too many concurrent logins - rate limited.");
+            return;
+        }
+
+        try {
+            String username = (String) record.getValue().get("username");
+            String password = (String) record.getValue().get("password");
+            String sessionId = (String) record.getValue().get("sessionId");
+
+            try{
+                AuthRequest authRequest = new AuthRequest(username, password, sessionId);
+                AuthResponse authResponse = authService.login(authRequest);
+
+                messagingTemplate.convertAndSendToUser(sessionId, "/queue/login-result", authResponse);
+
+                //Save login result to Redis
+                String resultKey = "login:result:" + sessionId;
+                Map<String, String> resultMap = new HashMap<>();
+                resultMap.put("status" ,authResponse.getStatus().toString());
+                resultMap.put("username", authResponse.getUsername());
+                resultMap.put("token", authResponse.getToken());
+                resultMap.put("id", String.valueOf(authResponse.getId()));
+                resultMap.put("role", String.valueOf(authResponse.getRole()));
+                resultMap.put("message", authResponse.getMessage());
+
+                redisTemplate.opsForHash().putAll(resultKey, resultMap);
+                redisTemplate.expire(resultKey, Duration.ofMinutes(1));
+            } catch (Exception e) {
+                handleError(record, e);
+            }finally {
+                redisTemplate.opsForStream().acknowledge(STREAM_KEY, GROUP, record.getId());
+            }
+        }finally {
+            rateLimiter.release();
+        }
+    }
+
+    //Logic handle error request login
+    private void handleError (MapRecord<String, Object, Object> record, Exception e){
+        String username = (String) record.getValue().get("username");
+        String sessionId = (String) record.getValue().get("sessionId");
+
+        AuthResponse errorResponse = new AuthResponse(
+                null, null, username, null, Status.FAIL, e.getMessage()
+        );
+        messagingTemplate.convertAndSendToUser(sessionId, "/queue/login-result", errorResponse);
+
+        String resultKey = "login:result:" + sessionId;
+        Map<String, String> resultMap = new HashMap<>();
+        resultMap.put("status", Status.FAIL.toString());
+        resultMap.put("username", username);
+        resultMap.put("token", "");
+        resultMap.put("id", "");
+        resultMap.put("role", "");
+        resultMap.put("message", e.getMessage());
+
+        redisTemplate.opsForHash().putAll(resultKey, resultMap);
+        redisTemplate.expire(resultKey, Duration.ofMinutes(1));
     }
 }
