1085229982dbafc1c8203b58ad89de4141fb93b2
nhannguyen <thanhnhanna102003@gmail.com>
2025-10-31 19:31:20 +0700

    Set up Entites Note & MediaNote conditions

--- notesWeb/src/main/java/com/example/notesWeb/model/takeNotes/NoteMedia.java
+++ notesWeb/src/main/java/com/example/notesWeb/model/takeNotes/NoteMedia.java
@@ -1,5 +1,6 @@
 package com.example.notesWeb.model.takeNotes;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -14,12 +15,15 @@ public class NoteMedia {
     @GeneratedValue
     private UUID id;
 
+    @Column(nullable = false, length = 500)
     private String url;
 
     @Enumerated(EnumType.STRING)
+    @Column(nullable = false, length = 20)
     private MediaType type;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "noteId", nullable = false)
+    @JsonBackReference
     private Notes notes;
 }
