0183f146b137a10c0898b046c96a55c067759370
nhannguyen <thanhnhanna102003@gmail.com>
2025-11-06 14:58:08 +0700

    Add class webCORSConfig handle port from client for call api

---
+++ notesWeb/src/main/java/com/example/notesWeb/security/webCORSConfig.java
@@ -0,0 +1,24 @@
+package com.example.notesWeb.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class webCORSConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry corsRegistry) {
+                corsRegistry.addMapping("/**")
+                        .allowedOrigins("*")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
