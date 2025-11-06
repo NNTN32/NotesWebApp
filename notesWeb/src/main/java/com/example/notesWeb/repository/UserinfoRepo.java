4e312b4fe7e28332303881d86f2d59ed6641d319
nhannguyen <thanhnhanna102003@gmail.com>
2025-11-06 15:19:35 +0700

    Change condition allowed pattern at webCORSConfig, delete entity class userInfo

--- notesWeb/src/main/java/com/example/notesWeb/repository/UserinfoRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.notesWeb.repository;
-
-import com.example.notesWeb.model.UserInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface UserinfoRepo extends JpaRepository<UserInfo, UUID> {
-}
