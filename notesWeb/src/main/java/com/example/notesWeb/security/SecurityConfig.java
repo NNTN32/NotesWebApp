f031ab5dbd85d8878f2a68daf9735db7844fea90
nhannguyen <thanhnhanna102003@gmail.com>
2026-03-16 10:57:12 +0700

    Authorize api ports

--- notesWeb/src/main/java/com/example/notesWeb/security/SecurityConfig.java
+++ notesWeb/src/main/java/com/example/notesWeb/security/SecurityConfig.java
@@ -52,7 +52,11 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html",
-                                "/api/auth/**"
+                                "/api/auth/**",
+                                "/notes/**",
+                                "/media/**",
+                                "/todo/**",
+                                "/reminder/**"
                         ).permitAll()
                         .anyRequest().authenticated()
                 )
