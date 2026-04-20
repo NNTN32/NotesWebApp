29f808cb78fd24030e8170a842a27841891b2ccb
nhannguyen <thanhnhanna102003@gmail.com>
2026-04-20 22:54:58 +0700

    Adding port domain for server

--- notesWeb/src/main/java/com/example/notesWeb/security/apiConfig.java
+++ notesWeb/src/main/java/com/example/notesWeb/security/apiConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +17,8 @@ public class apiConfig {
         return new OpenAPI()
                 .info(new Info().title("Note App API").version("1.0"))
 //                .addSecurityItem(new SecurityRequirement().addList(schemeName))
+                .addServersItem(new Server().url("https://server.nhannotes.id.vn").description("Production Server"))
+                .addServersItem(new Server().url("http://localhost:8081").description("Local Server"))
                 .components(new Components()
                         .addSecuritySchemes(schemeName, new SecurityScheme()
                                 .name(schemeName)
